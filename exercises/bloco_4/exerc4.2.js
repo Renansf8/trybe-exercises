eeef1b3380c0e285c7b344cadffab4e92b4934a3
Renan Ferreira <renansantiago2013@hotmail.com>
2021-04-22 18:32:22 -0300

    Adiciona os programas 5, 6, 7, 8 e 9

--- exercises/bloco_4/exerc4.2.js
+++ exercises/bloco_4/exerc4.2.js
@@ -75,3 +75,60 @@ else if (angle1 + angle2 + angle3 === 180) {
 }
 
 //Programa 6
+let piece = "RAINHA";
+let pieceLower = piece.toLowerCase(); 
+
+switch(pieceLower) {
+  case "peao": 
+      console.log("Para frente")
+      break;
+
+    case "torre":
+      console.log("Horizontal e vertical")
+      break;
+
+    case "rainha":
+      console.log("Para todos os lados")
+      break;
+}
+
+//Programa 7
+let note = 60;
+
+if (note >= 90) {
+  console.log("A");
+} else if (note < 90 && note >= 80) {
+  console.log("B");
+} else if (note < 80 && note >= 70) {
+  console.log("C")
+} else if (note < 70 && note >= 60) {
+  console.log("D")
+} else if (note < 60 && note >= 50) {
+  console.log("E");
+} else (
+  console.log("F")
+)
+
+//PROGRAMA 8
+let num1 = 3;
+let num2 = 5;
+let num3 = 11;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+//PROGRAMA 9
+let num1 = 8;
+let num2 = 10;
+let num3 = 18;
+
+if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+//PROGRAMA 10

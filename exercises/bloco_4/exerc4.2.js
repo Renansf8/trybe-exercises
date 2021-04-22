edd576a09e144c8bd724f0d8c48f463b57ed0e89
Renan Ferreira <renansantiago2013@hotmail.com>
2021-04-22 19:06:14 -0300

    Adiciona programas 10 e 11

--- exercises/bloco_4/exerc4.2.js
+++ exercises/bloco_4/exerc4.2.js
@@ -132,3 +132,44 @@ if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0) {
 }
 
 //PROGRAMA 10
+let custo = 80;
+let valorVenda = 130;
+let custoTotal = custo + ((20/100) * custo);
+let lucro = valorVenda - custoTotal;
+
+if (custo < 0 || valorVenda < 0) {
+  console.log("Valor inválido")
+} else {
+  console.log(lucro * 1000);
+}
+
+//PROGRAMA 11
+let salarioBruto = 5000;
+let INSS;
+let IR;
+
+if (salarioBruto <= 1556.94) {
+  INSS = ((8/100) * salarioBruto);
+} else if (salarioBruto > 1556.95 && salarioBruto <= 2594.92) {
+  INSS = ((9/100) * salarioBruto);
+} else if (salarioBruto > 2594.93 && salarioBruto <= 5189.82) {
+  INSS = ((11/100) * salarioBruto);
+} else {
+  INSS = 570.88;
+}
+
+let salarioBase = salarioBruto - INSS;
+
+if (salarioBase > 1903.99 && salarioBase <= 2826.65) {
+  IR = ((7.5/100 * salarioBase) - 142.80);
+} else if (salarioBase > 2826.65 && salarioBase <= 3751.05) {
+  IR = ((15/100 * salarioBase) - 354,80);
+} else if (salarioBase > 3751.06 && salarioBase <= 4664.68) {
+  IR = ((22.5/100 * salarioBase) - 636.13);
+} else {
+  IR = ((27.5/100 * salarioBase) - 869.36);
+}
+
+let salarioLiquido = salarioBruto - (INSS + IR);
+
+console.log(salarioLiquido);

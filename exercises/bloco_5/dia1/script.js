b3de45d9db9775590005734aead3ee1a1a831709
Renan Ferreira <renansantiago2013@hotmail.com>
2021-04-29 15:50:07 -0300

    Adiciona os exercicios do dia 1 do bloco 5

---
+++ exercises/bloco_5/dia1/script.js
@@ -0,0 +1,15 @@
+let emergencyContainers = document.getElementsByClassName('emergency-tasks');
+emergencyContainers[0].style.backgroundColor = 'pink';
+let emergencyTitles = document.querySelectorAll('.emergency-tasks h3');
+emergencyTitles[0].style.backgroundColor = 'purple';
+emergencyTitles[1].style.backgroundColor = 'purple';
+emergencyTitles[0].style.color = 'white';
+emergencyTitles[1].style.color = 'white';
+
+let noEmergencyContainers = document.getElementsByClassName('no-emergency-tasks');
+noEmergencyContainers[0].style.backgroundColor = 'yellow';
+let noEmergencyTitles = document.querySelectorAll('.no-emergency-tasks h3');
+noEmergencyTitles[0].style.backgroundColor = 'black'; 
+noEmergencyTitles[1].style.backgroundColor = 'black'; 
+noEmergencyTitles[0].style.color = 'white'; 
+noEmergencyTitles[1].style.color = 'white'; 

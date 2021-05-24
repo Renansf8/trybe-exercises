b9cbe2b64aa82c57b30dff5c64c11db8abec538f
Renan Ferreira <renansantiago2013@hotmail.com>
2021-05-24 11:04:29 -0300

    Adiciona a segunda parte de exercicios do dia 7.3

---
+++ exercises/bloco_7/dia3/exercise5.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+// implemente seus testes aqui
+assert.deepStrictEqual(obj1, obj2, 'O objeto1 é idêntico ao objeto2');
+assert.notDeepStrictEqual(obj1, obj3, 'O objeto1 não é idêntico ao objeto3');

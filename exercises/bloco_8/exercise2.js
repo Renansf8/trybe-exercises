362c582755683e28347e8a40a01ac64ed7eed33e
Renan Ferreira <renansantiago2013@hotmail.com>
2021-05-25 19:15:26 -0300

    Adiciona o exercicio 2 do dia 8.1

---
+++ exercises/bloco_8/exercise2.js
@@ -0,0 +1,10 @@
+// Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const verifyPrize = (number, checkNumber) => {
+  const prizeNumber = Math.floor((Math.random() * 5) + 1);
+  return checkNumber(number, prizeNumber) ? 'Parabens, Você ganhou!!!' : 'Tente Novamente :/';
+};
+
+const checkNumber = (myNumber, number) => myNumber === number;
+
+console.log(verifyPrize(2, checkNumber));

bc2860257a5db10edd3de645e9163a4b727865a9
Renan Ferreira <renansantiago2013@hotmail.com>
2021-06-14 18:58:54 -0300

    Resolve o exercicio 2 da parte 2

--- exercises/bloco_10/dia1/playground.test.js
+++ exercises/bloco_10/dia1/playground.test.js
@@ -1,7 +1,15 @@
-const { compareTrue } = require('./playground');
-const { test, expect } = require('@jest/globals');
+const { compareTrue, calcArea } = require('./playground');
+const { test, expect, describe } = require('@jest/globals');
 
-test('compare two values', () => {
-  expect(compareTrue(1, 1)).toBe(true);
-  expect(compareTrue(0, 1)).not.toBe(true);
+describe('Playground functions', () => {
+  test('compare two values', () => {
+    expect(compareTrue(1, 1)).toBe(true);
+    expect(compareTrue(0, 1)).not.toBe(true);
+  });
+
+  test('verify area', () => {
+    expect(calcArea(2, 4)).toBe(4);
+    expect(calcArea(3, 4)).toBe(6);
+  });
 });
+

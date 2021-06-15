d0e1bd57f051d368234b1bbc2d57423646a79e4b
Renan Ferreira <renansantiago2013@hotmail.com>
2021-06-15 19:12:03 -0300

    Adiciona o exercicio 2 do dia 10.2

--- exercises/bloco_10/dia2/exercise.test.js
+++ exercises/bloco_10/dia2/exercise.test.js
@@ -1,4 +1,4 @@
-const { uppercase } = require('./exercise.js');
+const { uppercase, getUserName } = require('./exercise.js');
 const { it, describe, expect } = require('@jest/globals');
 
 //Exercício 1 - Escreva um teste que verifique a chamada do callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas. Lembre-se de ter cuidado com os falsos positivos em testes assíncronos.
@@ -8,5 +8,25 @@ describe('Testa a função uppercase', () => {
       expect(str).toBe('TEST');
       done()
     })
+  });
+});
+
+//Exercício 2 - Utilizando a sintaxe de Promise , faça um teste que verifique o resultado da função getUserName para o caso em que o usuário é encontrado, e também um teste para o caso em que o usuário não é encontrado.
+
+describe('Caso o ID seja encontrado', () => {
+  it('Retorna o nome do usuário', () => {
+    expect.assertions(1);
+    return getUserName('4').then(name => {
+      expect(name).toBe('Mark')
+    });
+  });
+})
+
+describe('Caso o ID não seja encontrado', () => {
+  it('Retorna um erro', () => {
+    expect.assertions(1);
+    return getUserName(1).catch(name => {
+      expect(name).toEqual({ error: 'User with 1 not found.' })
+    });
   });
 });

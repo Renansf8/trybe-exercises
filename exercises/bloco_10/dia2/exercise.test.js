4ba5d7cb5a280422fa73c7a5c77c7392b4faf5fd
Renan Ferreira <renansantiago2013@hotmail.com>
2021-06-15 21:54:37 -0300

    Adiciona o exercicio 5 do dia 10.2

--- exercises/bloco_10/dia2/exercise.test.js
+++ exercises/bloco_10/dia2/exercise.test.js
@@ -63,11 +63,41 @@ const getRepos = (url) => {
     });
 };
 
-it('gets a list of repositories names', () => {
-  const url = 'https://api.github.com/orgs/tryber/repos';
+// it('gets a list of repositories names', () => {
+//   const url = 'https://api.github.com/orgs/tryber/repos';
 
-  return getRepos(url).then(result => {
-    expect(result).toContain('sd-01-week4-5-project-todo-list');
-    expect(result).toContain('sd-01-week4-5-project-meme-generator');
-  });
+//   return getRepos(url).then(result => {
+//     expect(result).toContain('sd-01-week4-5-project-todo-list');
+//     expect(result).toContain('sd-01-week4-5-project-meme-generator');
+//   });
+// });
+
+it('gets a list of repositories names', async () => {
+  const data = await getRepos('https://api.github.com/orgs/tryber/repos');
+
+  expect(data).toContain('sd-01-week4-5-project-todo-list');
+  expect(data).toContain('sd-01-week4-5-project-meme-generator');
 });
+
+//Exercício 5 - Para este exercício, tente adivinhar a saída dos console.log dos testes abaixo sem executá-los, e veja se compreendeu bem o funcionamento do beforeEach e do afterEach .
+
+beforeEach(() => console.log('1 - beforeEach'));
+afterEach(() => console.log('1 - afterEach'));
+
+test('', () => console.log('1 - test'));
+
+describe('Scoped / Nested block', () => {
+  beforeEach(() => console.log('2 - beforeEach'));
+  afterEach(() => console.log('2 - afterEach'));
+
+  test('', () => console.log('2 - test'));
+});
+
+// 1 - beforeEach
+// 1 - test
+// 1 - afterEach
+// 1 - beforeEach
+// 2 - beforeEach
+// 2 - test
+// 2 - afterEach
+// 1 - afterEach
